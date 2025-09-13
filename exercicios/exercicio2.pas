44a97f68c92fb8c1b19b3fd0b013fae217eab677
Person Arruda <41989037+ArrudaGit@users.noreply.github.com>
2025-09-13 08:44:03 -0300

    Update exercicio2.pas

--- exercicios/exercicio2.pas
+++ exercicios/exercicio2.pas
@@ -4,16 +4,17 @@
   Criado por: @programacaomentoria
 }  
 program exercicio2;
-	//lugar no programa onde declaro variaveis
-	//var
-		//nome_variavel: tipo
-		//os tipos podem ser:
-		// 	integer (numero inteiros), real (numeros com decimais)
-		//	boolean (tipos binarios - true/false)
-		//  char (um caractere)
-		//	string (cadeia de caracteres)
-begin:
-	//write - comando que escreve na tela
-	//readln - comando que pega a informacao digitada e adiciona em uma variavel. 
-	//			A variavel precisa ser passada como parametro da funcao readln, ex.: readln(nome_variavel)
+
+var	
+nome: string;
+idade: integer;
+
+begin
+    write('Qual é o seu nome? ');
+    readln(nome);
+    write('Qual a sua idade, ', nome, ' ? ');
+    readln(idade);
+    writeln('Prazer em te conhecer, ', nome, '. Você tem ', idade, ' anos.');
+end.
+    
 end.
