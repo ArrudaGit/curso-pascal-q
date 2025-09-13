69183b06a042c5a2096378ebf4aa8f9e6bdc8e31
Person Arruda <41989037+ArrudaGit@users.noreply.github.com>
2025-09-13 09:06:11 -0300

    Update exercicio3.pas

--- exercicios/exercicio3.pas
+++ exercicios/exercicio3.pas
@@ -5,6 +5,28 @@
 }  
 program exercicio3;
 
-begin:
+var
+nome: string;
+nota1, nota2, nota3, nota4, media: real;
 
+begin
+    Write('Nome do aluno: ');
+    readln(nome);
+    
+    Write('Nota 1: ');
+    readln(nota1);
+    Write('Nota 2: ');
+    readln(nota2);
+    Write('nota 3: ');
+    readln(nota3);
+    Write('Nota 4: ');
+    readln(nota4);
+    
+media := (nota1 + nota2 + nota3 + nota4) / 4;
+
+    writeln('');
+    writeln('Aluno: ', nome);
+    writeln('Média final: ', media:0:2); //0:2 calculo final com duas casas decimais
 end.
+
+	
