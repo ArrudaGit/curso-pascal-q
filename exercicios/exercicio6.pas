432f28fe7dda78b492df687de1488bc925424270
Person Arruda <41989037+ArrudaGit@users.noreply.github.com>
2025-09-14 17:36:02 -0300

    Update exercicio6.pas

--- exercicios/exercicio6.pas
+++ exercicios/exercicio6.pas
@@ -3,9 +3,21 @@
 						Como calcular: A = PI . R ao quadrado
   Data da criacao: 2025-08-28
   Criado por: @programacaomentoria
-}  
+}
 program exercicio6;
 
-begin:
+const
+    Pi = 3.1415; //Definir o valor manualmente sem biblioteca.
 
+var
+raio, area: Real;
+
+begin
+    writeln('Digite o raio do circulo: ');
+    readln(raio);
+    
+// Fórmula para cálculo: A = PI . R
+    area := Pi * (raio * raio); 
+    
+    writeln('A area do circulo com raio ', raio:0:2, ' é: ', area:0:2, '');
 end.
